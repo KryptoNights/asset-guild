a563a1d3eae182364bbcfe18e10f38cbeff58b7e
debjit <debjitbhowal.db@gmail.com>
2024-09-21 10:58:20 +0800

    eth sign integrated

--- backend/contracts/AssetGuild.sol
+++ backend/contracts/AssetGuild.sol
@@ -1,93 +1,119 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+// import {ISP} from "sign-protocol/interfaces/ISP.sol";
+// import {Attestation} from "sign-protocol/models/Attestation.sol";
+// import {DataLocation} from "sign-protocol/models/DataLocation.sol";
 interface IWorldID {
 	/// @notice Reverts if the zero-knowledge proof is invalid.
 	/// @param root The of the Merkle tree
-	/// @param groupId The id of the Semaphore group
-	/// @param signalHash A keccak256 hash of the Semaphore signal
-	/// @param nullifierHash The nullifier hash
-	/// @param externalNullifierHash A keccak256 hash of the external nullifier
-	/// @param proof The zero-knowledge proof
-	/// @dev  Note that a double-signaling check is not included here, and should be carried by the caller.
-	function verifyProof(
-		uint256 root,
-		uint256 groupId,
-		uint256 signalHash,
-		uint256 nullifierHash,
-		uint256 externalNullifierHash,
-		uint256[8] calldata proof
-	) external view;
-}
-library ByteHasher {
-	/// @dev Creates a keccak256 hash of a bytestring.
-	/// @param value The bytestring to hash
-	/// @return The hash of the specified value
-	/// @dev `>> 8` makes sure that the result is included in our field
-	function hashToField(bytes memory value) internal pure returns (uint256) {
-		return uint256(keccak256(abi.encodePacked(value))) >> 8;
-	}
-}
-contract Shutter {
-    using ByteHasher for bytes;
-    IWorldID internal immutable worldId;
-    mapping(uint256 => bool) internal nullifierHashes;
-    error DuplicateNullifier(uint256 nullifierHash);
-    uint256 internal immutable groupId = 1;
-    uint256 internal immutable externalNullifierHash;
-    event Verified(uint256 nullifierHash);
-    struct Content {
-        uint256 price;
-        uint64 purchaseCount;
-    }
-    mapping(string => Content) public allContent;
     struct Creator {
         mapping(string => string) hashmap;
         string[] previews;
-        bool verified;
+        uint8 verified;
     }
     mapping(address => Creator) creatorsStore;
-    event NewUpload (
-        string contentHash,
-        address creator,
+
         uint256 timestamp
     );
-	constructor(IWorldID _worldId, string memory _appId, string memory _actionId) {
-		worldId = _worldId;
-		externalNullifierHash = abi.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId).hashToField();
-	}
-	function verifyAndExecute(address signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public {
+
+	event PaidForContent (
+		address buyer,
+		address creator,
+		string contentHash,
+		uint256 timestamp,
+		uint256 price
+	);
+    struct Buyer {
+        mapping(string => string) hashmap;
+        string[] previews;
+    }
+	mapping(address => Buyer) buyersStore;
+
+	mapping(string => bool) public globalHashBase;
+	uint64 public schemaId;
+	// ISP public immutable isp;
+	// mapping(address => bytes32) public claims;
+	// constructor(
+		// IWorldID _worldId, 
+		// string memory _appId, 
+		// string memory _actionId,
+		// address _sign_deployed_addr, // 0x4e4af2a21ebf62850fD99Eb6253E1eFBb56098cD
+		// uint64 _schemaId // 239
+	// ) {
+		// worldId = _worldId;
+		// externalNullifierHash = abi.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId).hashToField();
+		// isp = ISP(_sign_deployed_addr);
+		// schemaId = _schemaId;
+	// }
+	function verifyOrbAndExecute(address signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public returns (bool) {
 		// First, we make sure this person hasn't done this before
 		if (nullifierHashes[nullifierHash]) revert DuplicateNullifier(nullifierHash);
-		// We now verify the provided proof is valid and the user is verified by World ID
-		worldId.verifyProof(
-			root,
-			groupId,
-			abi.encodePacked(signal).hashToField(),
-			nullifierHash,
-			externalNullifierHash,
-			proof
-		);
-		// We now record the user has done this, so they can't do it again (proof of uniqueness)
-		nullifierHashes[nullifierHash] = true;
+
+
 		// Finally, execute your logic here, for example issue a token, NFT, etc...
 		// Make sure to emit some kind of event afterwards!
-        creatorsStore[signal].verified = true;
+        creatorsStore[signal].verified = 2;
+
 		emit Verified(nullifierHash);
+        return true;
 	}
-    function addHash(string memory _a, string memory _b) public {
-        require(creatorsStore[msg.sender].verified, "Creator not verified");
-        Creator storage creator = creatorsStore[msg.sender];
-        
-        creator.hashmap[_a] = _b;
-        creator.previews.push(_a);
-        emit NewUpload(_a, msg.sender, block.timestamp);
+
+    function lightVerify(address creator) public returns(bool) {
+        creatorsStore[creator].verified = 1;
+        return true;
     }
+    // function verificationCallback(
+    //     address sender,
+    //     bytes32 claimId,
+    //     bytes32 postStateDigest,
+    //     bytes calldata seal
+    // ) public {
+    //     if (sender == address(0))
+    //         revert InvalidClaim("Invalid recipient address");
+    //     if (claimId == bytes32(0)) revert InvalidClaim("Empty claimId");
+    //     if (
+    //         !verifier.verify(
+    //             seal,
+    //             imageId,
+    //             postStateDigest,
+    //             sha256(abi.encode(sender, claimId))
+    //         )
+    //     ) {
+    //         revert InvalidClaim("Invalid proof");
+    //     }
+        
+    // }
+    function uploadContent(string memory _a, string memory _b) public {
+        require(creatorsStore[msg.sender].verified > 0, "Creator not verified");
+		require(!globalHashBase[_a], "Content already exists");
+		globalHashBase[_a] = true;
+
+        Creator storage creator = creatorsStore[msg.sender];
+
     function getHash(address _profile, string memory _a) public view returns (string memory) {
         return creatorsStore[_profile].hashmap[_a];
     }
-    
+    function verificationLevel(address _profile) public view returns(uint) {
+        Creator storage creator = creatorsStore[_profile];
+        return creator.verified;
+    }
+
     // 0x0AF104C5A40C498576859dC4169A1e4b1b7Cd6b7
+
     // function getProfileDetails(address creator) public view {
-        
+
     // }
+    function buyContent(address creator, string memory previewHash) public payable returns (bool) {
+		require(msg.value >= allContent[previewHash].price, "Provide enough funds");
+		// assert not already bought
+		require(keccak256(bytes(buyersStore[msg.sender].hashmap[previewHash])) == keccak256(bytes("")), "Already bought");
+		allContent[previewHash].purchaseCount += 1;
+		// Transfer the funds to the creator
+		payable(creator).transfer(msg.value);
+		emit PaidForContent(msg.sender, creator, previewHash, block.timestamp, msg.value);
+		buyersStore[msg.sender].hashmap[previewHash] = previewHash;
+		buyersStore[msg.sender].previews.push(previewHash);
+		return true;
+	}
 }
